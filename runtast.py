8569d76aa9b9f3f60c3b4e34a3a15f9bae87fc55
lhuaizhong <lhuaizhong@gmail.com>
2018-08-13 08:35:05 +0800

    Update something.

--- runtast.py
+++ runtast.py
@@ -7,6 +7,7 @@ import os,sys
 import shutil
 
 import csv
+import json
 
 import time
 
@@ -17,6 +18,9 @@ import datetime
 
 import logging
 import traceback
+
+from custom.custom1 import Custom1SSH
+#import netmiko
 
 def comparefile(out_file, out_file_pre, out_file_diff):
     fa = open(out_file,'r')
@@ -38,6 +42,8 @@ def comparefile(out_file, out_file_pre, out_file_diff):
 
 def main():
     start_time = time.clock()
+    #print(netmiko.ssh_dispatcher.CLASS_MAPPER)
+    #print(ssh_dispatcher.platforms)
 
     logging.basicConfig(filename='logging.txt', format="%(asctime)s;%(levelname)s;%(message)s", level=logging.DEBUG)
 
@@ -54,15 +60,30 @@ def main():
     print('There are %d tasks to run.' % len(tasks))
 
     for task in tasks: ###
+        if not task[0].strip() or not task[2].strip():
+            continue
         cmd = ''
         try:
+            para = {
+                'hostname':task[0],
+                'ip':task[2],
+                'username':task[3],
+            }
+            if task[8]:
+                #j = json.loads(task[8])
+                j = eval(task[8])
+                para.update(j)
             isoutput = False
             if 'y' in task[6].lower():
                 isoutput = True
-            cmd_file = 'commands%s%s.txt' % (os.sep, task[5])
-            out_file = 'output%s%s.txt' % (os.sep, task[0])
-            out_file_pre = 'output%s%s_pre.txt' % (os.sep, task[0])
-            out_file_diff = 'output%s%s_diff.txt' % (os.sep, task[0])
+
+            path = task[7].strip() or 'output'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            cmd_file = os.path.join('commands', task[5]+'.txt')
+            out_file = os.path.join(path, task[0]+'.txt')
+            out_file_pre = os.path.join(path, task[0]+'_pre.txt')
+            out_file_diff = os.path.join(path, task[0]+'_diff.txt')
             logging.debug('Run %s to %s using file %s' % (task[0], task[2], cmd_file))
             print('Run %s to %s using file %s' % (task[0], task[2], cmd_file))
             commands = []
@@ -75,16 +96,36 @@ def main():
 
             # Connect to device.
             net_connect = ConnectHandler(device_type=task[1], ip=task[2], username=task[3], password=task[4])
+            #net_connect.set_base_prompt(pri_prompt_terminator=':')
+            more = ''
             for cmd in commands: ###
+                cmd = cmd.format(**para)
                 cmd = cmd.strip().replace('\\n','\n')
-                print('command>>' + cmd)
+                expect_string=''
+                if '@@' in cmd:
+                    ss = cmd.split('@@')
+                    cmd = ss[0]
+                    expect_string = ss[1].strip()
+                if cmd[:2]=='##':
+                    more = cmd[2:].strip()
+                    print('Paging string: '+more)
+                    continue
+                if not expect_string:
+                    print('command>>' + cmd)
+                else:
+                    print('command>> %s  =>%s' % (cmd, expect_string))
                 #output = net_connect.send_command(cmd)
-                output = net_connect.send_command(cmd, max_loops=10000, auto_find_prompt=False, strip_prompt=False, strip_command=False)
+                #output = net_connect.send_command(cmd, max_loops=10000, auto_find_prompt=False, strip_prompt=False, strip_command=False, expect_string=expect_string)
+                output = net_connect.send_command(cmd, max_loops=100, auto_find_prompt=False, strip_prompt=False, strip_command=False, expect_string=expect_string)
                 if len(output)>60:
                     print('output>>' + output[:60]+'...')
                 else:
                     print('output>>' + output)
-                outputs += [output]
+                if not more:
+                    outputs += [output]
+                else:
+                    outputs += [output.replace(more, '')]
+                    #outputs += [output]
             net_connect.disconnect()
 
             # Storing output
